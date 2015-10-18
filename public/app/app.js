e5326f3f72a174b5ecebf33446940cf5798308d3
Martin Martinov <martin@mmartinov.com>
2015-10-18 14:15:28 +0100

    Adding some icons.

--- public/app/app.js
+++ public/app/app.js
@@ -19,12 +19,17 @@ angular
     $httpProvider.interceptors.push('AuthInterceptor');
 
 		$mdIconProvider
-		//.defaultIconSet("./assets/svg/avatars.svg", 128)
-		.icon("menu", "./assets/svg/menu.svg", 24);
+			//.defaultIconSet("./assets/svg/avatars.svg", 128)
+			.icon("account-box", "./assets/svg/ic_account_box_black_24px.svg", 24)
+			.icon("assessment", "./assets/svg/ic_assessment_black_24px.svg", 24)
+			.icon("assignment", "./assets/svg/ic_assignment_black_24px.svg", 24)
+			.icon("close", "./assets/svg/ic_close_black_24px.svg", 24)
+			.icon("home", "./assets/svg/ic_home_black_24px.svg", 24)
+			.icon("menu", "./assets/svg/menu.svg", 24);
 
 		// change some theming options
 		$mdThemingProvider.theme('default')
       .primaryPalette('lime')
-      .accentPalette('amber');
+      .accentPalette('light-green');
 
 	});
